ef5bbbb014a0865e5816a36e976ffb4ee97eedb8
VirgilioConti <virgiliconti@gmail.com>
2023-04-01 13:29:30 +0200

    Initialization repository with empty project

---
+++ src/principale/ClassToExecuteBK.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package principale;
+
+/**
+ * @author Virgilio
+ *
+ */
+public class ClassToExecuteBK {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
